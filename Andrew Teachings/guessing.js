fac65815e2e10d62b007c02f5f30a21567d69725
XDLegends <58534916+XDLegends@users.noreply.github.com>
2020-09-15 15:09:36 -0400

    Trying to make my first web page game

--- Andrew Teachings/guessing.js
+++ Andrew Teachings/guessing.js
@@ -1,13 +1,29 @@
-prompt('You have 10 guesses. Guess 1-10');
+alert('You have infinite guesses.');
 
 var answer = Math.floor(Math.random()*100);
-var guess = prompt("What's your guess?");
+var guess = "";
 
-for(i = 0; i < 10; i++){
-          if(answer == guess){
-            alert("You guessed correctly");
-            break;
-          } else{
-            guess = alert("Please Try Again");
-          }
+while (guess != answer){
+      guess = prompt("what is you guess?");
+      if(guess > answer)
+      msg = "Your guess is too high";
+      else if (guess < answer)
+      msg = "Your guess is too low";
+      else
+      msg = "You Win";
+      alert(msg);
 }
+
+
+
+// else
+// msg = "You Win!";
+
+
+          //if(answer == guess){
+            //alert("You guessed correctly");
+            //break;
+          //} else{
+            //guess = alert("Please Try Again");
+          //}
+//}
